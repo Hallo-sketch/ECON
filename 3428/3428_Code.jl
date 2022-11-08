539e71f873d1a5ce882e7693d1485d42f5c18e49
Hallo-sketch <74075832+Hallo-sketch@users.noreply.github.com>
2022-11-08 16:25:30 -0600

    Still Broken

--- 3428/3428_Code.jl
+++ 3428/3428_Code.jl
@@ -4,10 +4,21 @@ Pkg.add("Plots")
 Pkg.add("DataFrames")
 Pkg.add("CSV")
 Pkg.add("StatsPlots")
+Pkg.add("StatsKit")
 
 using Plots, DataFrames, CSV, StatsPlots
 
 p = "//Users//liammartin//ECON//3428//Data//CORESTICKM159SFRBATL.csv"
 df1 = DataFrame(CSV.File(p))
 print(df1)
+
+
+plot(df1.DATE, df1.CORESTICKM159SFRBATL, labels=["CORESTICKM159SFRBATL"], legend=:topleft)
+
+p1 = "//Users//liammartin//ECON//3428//Data//fredgraph.csv"
+df2 = DataFrame(CSV.File(p1))
+print(df2)
+using StatsKit
+print(Matrix(df2[!, Not(:DATE)]))
+MultinomialLRTest(Matrix(df2[]))
 
